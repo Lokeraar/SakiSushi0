7aa0e3eee8c4b5d4ff0a858d1845f73fb38fb997
Lokeraar <93406324+Lokeraar@users.noreply.github.com>
2026-03-31 16:35:25 -0400

    Actualizar supabaseClient.js

--- admin/services/supabaseClient.js
+++ admin/services/supabaseClient.js
@@ -1,1 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
+const SUPABASE_URL = 'https://iqwwoihiiyrtypyqzhgy.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_m4WcF4gmkj1olAj95HMLlA_4yKqPFXm';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
