9bb122f20e6570e2fca44c3da01aa27818d1094a
Lokeraar <93406324+Lokeraar@users.noreply.github.com>
2026-03-31 16:39:09 -0400

    Actualizar inventoryStore.js

--- admin/stores/inventoryStore.js
+++ admin/stores/inventoryStore.js
@@ -1,1 +1,19 @@
 
+export const inventoryStore = {
+  items: [],
+  set(data) {
+    this.items = data;
+  },
+  updateItem(updated) {
+    const index = this.items.findIndex(i => i.id === updated.id);
+    if (index !== -1) {
+      this.items[index] = { ...this.items[index], ...updated };
+    }
+  },
+  addItem(item) {
+    this.items.push(item);
+  },
+  removeItem(id) {
+    this.items = this.items.filter(i => i.id !== id);
+  }
+};
