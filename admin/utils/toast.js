835e80739502c86bf91d731c307d8866d6516f07
Lokeraar <93406324+Lokeraar@users.noreply.github.com>
2026-04-01 22:51:01 -0400

    Delete admin/utils directory

--- admin/utils/toast.js
+++
@@ -1,8 +0,0 @@
-// admin/utils/toast.js
-export function showToast(message, type = 'success') {
-    const toast = document.createElement('div');
-    toast.className = `toast ${type}`;
-    toast.innerText = message;
-    document.body.appendChild(toast);
-    setTimeout(() => toast.remove(), 3000);
-}
